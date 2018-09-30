f7c62c3849dc051a0d5ecccc2b8a7bd5ba2cb3c9
Kevin Do <do.kevin.vo@gmail.com>
2018-09-29 23:46:37 -0700

    adds burger obj

--- models/burger.js
+++ models/burger.js
@@ -7,3 +7,23 @@ const orm = require(`../config/orm.js`);
 ├── models               ║
     └── burger.js <══════╝
 * * * * * * * * * * * * * * * * * * * * */
+
+var burger = {
+    selectAll: function(callback) {
+        orm.selectAll(`burgers`, function(res) {
+            callback(res);
+        });
+    },
+    insertOne: function(cols, vals, callback) {
+        orm.insertOne(`burgers`, cols, vals, function(res) {
+            callback(res);
+        });
+    },
+    updateOne: function(objColVals, condition, callback) {
+        orm.updateOne(`burgers`, objColVals, condition, function(res) {
+            callback(res);
+        });
+    }
+};
+
+module.exports = burger;
